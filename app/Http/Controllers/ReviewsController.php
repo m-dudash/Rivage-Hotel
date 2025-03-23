00783e229ace20cffac22d02040e4db3c8a03158
Mykhailo Dudash <dudasmika17@gmail.com>
2025-03-23 16:56:32 +0100

    Load reviews

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewsController extends Controller
 {
     public function index()
     {
-        return view('reviews');
+        $reviews = Review::all();
+        return view('reviews', ['reviews'=>$reviews]);
     }
 }
--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -7,18 +7,20 @@
     </div>
     <br><br><br>
 
+    @foreach($reviews as $review)
+        <div class="review">
+            <p class="thm" style="margin-left: 140px">✩ {{$review->rating}}</p>
+            <p style="margin-left: 14px; color: #500505; font-size: 23px">{{$review->user ? $review->user->name : 'Anonymous'}}</p>
+            <div style="width: 50%; margin-left: 140px">
+                <p>{{$review->content}}</p>
+            </div>
+            <div>
+                <p style="right: 70px; position: absolute; margin-top: 50px">{{$review->review_date}}</p>
+            </div>
+        </div>
+        <br><br><br><br>
+    @endforeach
 
-    <div class="review">
-        <p class="thm" style="margin-left: 140px">✩ 10</p>
-        <p style="margin-left: 14px; color: #500505; font-size: 23px">fakeUser</p>
-        <div style="width: 50%; margin-left: 140px">
-            <p>review text</p>
-        </div>
-        <div>
-            <p style="right: 70px; position: absolute; margin-top: 50px">10 march 2023</p>
-        </div>
-    </div>
-    <br><br><br><br>
 
     <br /><br /><br /><br />
     <p class="thm" style="font-size: 200%">
