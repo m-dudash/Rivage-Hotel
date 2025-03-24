b3422af0cbe974b0c23c7329f0703c0f614e9ad8
Mykhailo Dudash <dudasmika17@gmail.com>
2025-03-24 20:00:15 +0100

    Sanctum stateful configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 
 class User extends Model
 {
-
+    use HasApiTokens;
     protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token'];
 }
